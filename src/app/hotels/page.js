b4f074a9ef9037dab0158ceffa4ddb2de3fd4f20
Rishabh Angure <angurerishabh2002@gmail.com>
2025-05-02 13:00:29 +0530

    Initial commit

---
+++ src/app/hotels/page.js
@@ -0,0 +1,11 @@
+export default function Page(){
+      return (
+            <main>
+                  <div>
+                        <h1>
+                              Hotel Details
+                        </h1>
+                  </div>
+            </main>
+      )
+}
