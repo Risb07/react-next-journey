2eb63c6df8d247430275f4d37fe46f68478084bb
Rishabh Angure <103446993+Risb07@users.noreply.github.com>
2025-05-03 01:43:23 +0530

    Create Forms for Contact Route (updated)

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -13,7 +13,7 @@ export default function Contact() {
   return (
     <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
       <h1 className="text-2xl font-bold text-center mb-6">Contact us!</h1>
-      <form className="space-y-4">
+      <form className="space-y-4" action={submitForm}>
         <div>
           <label
             htmlFor="email"
